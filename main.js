7c759402f163de0705950f28ac1219e698f0db24
r3aldii <statovcireald@gmail.com>
2025-03-10 04:09:11 -0700

    comittt

---
+++ main.js
@@ -0,0 +1,24 @@
+var slideindex=1;
+
+function showslide(n){
+    var i;
+    var slides= document.getElementsByClassName("slide");
+}
+
+for(i=0; 1 <slideindex.length; i++){
+    slides[i].style.display="none";
+}
+
+if(n> sliders.length){
+    slideindex=1;
+
+}
+
+if(n<1){
+slideindex=slides.length;
+}
+
+slides[slideindex-1].style.display="block";
+
+show
+
